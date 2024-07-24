9bbcb77d0954ee39fdbc032f17d478422e2b082a
Fin M <qbres333@hotmail.com>
2024-07-24 14:09:10 -0600

    dark mode works!

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,3 +1,26 @@
+// Access mode button HTML element
+const themeSwitcher = document.querySelector('#light-dark-mode');
+const container = document.querySelector('.container');
+
+// Set default mode to light
+let mode = 'light';
+
+// Listen for a click event on mode button
+themeSwitcher.addEventListener('click', function () {
+  // If mode is light, apply dark background
+  if (mode === 'light') {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
+  }
+
+  // If mode is dark, apply light background
+  else {
+    mode = 'light';
+    container.setAttribute('class', 'light');
+  }
+});
+
+// access blog content fields
 const blogUser = document.querySelector('#username');
 const blogTitle = document.querySelector('#blog-title');
 const blogContent = document.querySelector('#content');
